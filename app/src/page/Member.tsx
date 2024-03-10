8a24f1fb85b98622fb1e1c6b968df79f0883d0ae
ntut-xuan <t109590031@ntut.org.tw>
2024-03-10 22:40:29 +0800

    Implement: Fix the role

--- app/src/page/Member.tsx
+++ app/src/page/Member.tsx
@@ -7,8 +7,37 @@ function Member(){
             <h2>實驗室成員</h2>
             <hr></hr>
             <div className="pb-4 d-flex flex-column gap-3">
-                <h4>Role 1</h4>
+                <h4>Professor</h4>
                 <div className="d-flex flex-row" style={{flexWrap: "wrap"}}>
+                    <div className="w-50 p-2">
+                        <Card className="p-3 d-flex flex-row gap-3">
+                            <div className="d-flex flex-column">
+                                <Avatar size="lg" className="my-auto" circle></Avatar>
+                            </div>
+                            <div className="d-flex flex-column text-start">
+                                <span className="fs-5 font-bold">Name</span>
+                                <span>Professor</span>
+                                <span>some@email.com</span>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+            <div className="pb-4 d-flex flex-column gap-3">
+                <h4>Guest</h4>
+                <div className="d-flex flex-row" style={{flexWrap: "wrap"}}>
+                    <div className="w-50 p-2">
+                        <Card className="p-3 d-flex flex-row gap-3">
+                            <div className="d-flex flex-column">
+                                <Avatar size="lg" className="my-auto" circle></Avatar>
+                            </div>
+                            <div className="d-flex flex-column text-start">
+                                <span className="fs-5 font-bold">Name</span>
+                                <span>Guest</span>
+                                <span>some@email.com</span>
+                            </div>
+                        </Card>
+                    </div>
                     <div className="w-50 p-2">
                         <Card className="p-3 d-flex flex-row gap-3">
                             <div className="d-flex flex-column">
@@ -24,7 +53,7 @@ function Member(){
                 </div>
             </div>
             <div className="pb-4 d-flex flex-column gap-3">
-                <h4>Role 2</h4>
+                <h4>Student</h4>
                 <div className="d-flex flex-row" style={{flexWrap: "wrap"}}>
                     <div className="w-50 p-2">
                         <Card className="p-3 d-flex flex-row gap-3">
@@ -33,7 +62,7 @@ function Member(){
                             </div>
                             <div className="d-flex flex-column text-start">
                                 <span className="fs-5 font-bold">Name</span>
-                                <span>Role 2</span>
+                                <span>Student</span>
                                 <span>some@email.com</span>
                             </div>
                         </Card>
@@ -45,7 +74,7 @@ function Member(){
                             </div>
                             <div className="d-flex flex-column text-start">
                                 <span className="fs-5 font-bold">Name</span>
-                                <span>Role 2</span>
+                                <span>Student</span>
                                 <span>some@email.com</span>
                             </div>
                         </Card>
@@ -57,7 +86,7 @@ function Member(){
                             </div>
                             <div className="d-flex flex-column text-start">
                                 <span className="fs-5 font-bold">Name</span>
-                                <span>Role 2</span>
+                                <span>Student</span>
                                 <span>some@email.com</span>
                             </div>
                         </Card>
