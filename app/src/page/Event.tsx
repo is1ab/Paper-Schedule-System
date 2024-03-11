54d54093c21680e2f88a8413778f5d63ec2e8840
ntut-xuan <t109590031@ntut.org.tw>
2024-03-11 19:58:04 +0800

    Chore: Change calendar style

--- app/src/page/Event.tsx
+++ app/src/page/Event.tsx
@@ -3,10 +3,8 @@ import { Calendar } from "rsuite";
 function Event(){
     return (
         <Container className="p-5 text-center">
-            <h2> 近期活動 </h2>
-            <Calendar>
-                
-            </Calendar>
+            <h2 className="pb-4"> 近期活動 </h2>
+            <Calendar bordered className="shadow rounded"></Calendar>
         </Container>
     )
 }
