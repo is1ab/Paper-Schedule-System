880ae6c24f8cdbaf35cb9fb468e24b4749d7b853
ntut-xuan <t109590031@ntut.org.tw>
2024-04-18 03:09:52 +0800

    Refactor: Home page

--- app/src/page/Home.tsx
+++ app/src/page/Home.tsx
@@ -1,20 +1,32 @@
-import { Alert, Container, Image } from "react-bootstrap"
+import { Container } from "react-bootstrap"
 import Logo from "../assets/logo.png"
+import { Alert, Image } from "antd"
 
 function Home() {
   return (
     <Container>
-        <div className="w-100 text-center d-flex flex-column" style={{height: "50vh"}}>
+        <div className="w-100 d-flex flex-column" style={{height: "50vh"}}>
             <hr className="w-100"></hr>
-            <div>
-                <Alert variant={'warning'} className="m-0">
-                    <span> 希望不要變成某週的攻防組主題 QAQ </span>
-                </Alert>
+            <div className="d-flex flex-column gap-3">
+                <Alert
+                    message="警告"
+                    description="還沒做完，極高機率會被打穿"
+                    type="error"
+                    showIcon
+                    closable
+                />
+                <Alert
+                    message="注意"
+                    description="希望不要變成某週的攻防組主題 QAQ"
+                    type="warning"
+                    showIcon
+                    closable
+                />
             </div>
             <hr className="w-100"></hr>
-            <div className="my-auto h-fit-content">
+            <div className="my-auto h-fit-content text-center">
                 <div className="w-25 mx-auto">
-                    <Image src={Logo} className="mx-auto w-100"></Image>
+                    <Image preview={false} src={Logo} className="mx-auto w-100"></Image>
                 </div>
                 <br/>
                 <h1>國立臺北科技大學 資訊安全實驗室</h1>
